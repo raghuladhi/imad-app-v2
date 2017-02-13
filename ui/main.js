014beab626b90304f7f6738e602014055640b60c
raghuladhi <raghuladhi123@gmail.com>
2017-02-13 18:35:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("changeid");
-element.innerHTML="Febraury 13-2017"
+element.innerHTML="Febraury 13-2017";
 var img = document.getElementById("changepos");
 img.onclick = function()
 {
-    img.style.marginleft="200px";
-}
+    img.style.marginleft='200px';
+};
