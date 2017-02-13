bde7f871236cb6239644d0fef833d8489443b06d
raghuladhi <raghuladhi123@gmail.com>
2017-02-13 18:41:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
-var img = document.getElementById("changepos");
-img.onclick = function()
+var img = document.getElementById('changepos');
+img.onclick = function ()
 {
     img.style.marginleft='200px';
 };
